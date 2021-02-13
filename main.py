8ef937e7a116d7b10363770b7ebcd59a2b09e6f3
HOME <Kanova2004@yandex.ru>
2021-02-13 08:49:34 +0500

    оно не работает. не пытайся. но там теперь есть недокнопка с недозакосом на изменение типа обзора. кароч потом обсудим реализацию

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from settings import *
 import requests
 import sys
 import os
+import pygame_gui
+import pygame
 
-
+WIDTH = 600
+HEIGHT = 450
 coords = '133.125502,-28.306867'
 scale = '50,30'
+sat = ['', '&l', 'l=sat%2Cskl&l']
 
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&spn={scale}&l=sat"
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&spn={scale}&l=sat{sat}"
 response = requests.get(map_request)
 
 if not response:
@@ -22,6 +26,24 @@ with open(map_file, "wb") as file:
     file.write(response.content)
 
 
+def start_screen():
+    manager = pygame_gui.UIManager((WIDTH, HEIGHT), os.path.join('data', 'menu_theme.json'))
+    outlook_btn = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((WIDTH // 2 - 75, HEIGHT // 3 + 55), (150, 50)),
+        text='спутник',
+        manager=manager
+    )
+    while True:
+        sat_choice = sat[0]
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == outlook_btn:
+                        sat_choice = sat[1]
+            manager.process_events(event)
+        manager.draw_ui(screen)
+        pygame.display.flip()
+
 pg.init()
 sc = pg.display.set_mode(size)
 
@@ -34,5 +56,4 @@ while pg.event.wait().type != pg.QUIT:
 pg.quit()
 
 os.remove(map_file)
-
 
